002ef8ad40e8b53bdfb7eb2b91646936312b4b88
c1affa <michele.ciaffarafa@gmail.com>
2023-01-30 16:03:52 +0100

    chore: interface improved

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,13 @@ import GA from './components/GA.vue'
 </script>
 
 <template>
+  <h2>Genetic Algorithm for Multiprocessor Scheduling</h2>
+  <p>
+    This simple web application allow to visualize the evolution of a genetic algorithm population applied to the multiprocessor scheduling optimization problem.
+  </p>
+  <p>
+    The interface allow to set the problem and the algorithm parameter. It's possible to interact with the algorithm during the exectution, tuning the number of machines, the mutation rate and the selection split ratio.
+  </p>
   <GA />
 </template>
 
--- src/components/GA.vue
+++ src/components/GA.vue
@@ -25,7 +25,7 @@ GA.eventListener.on('status-updated',(status)=>{
 
   for( let m in status.population){
     let setY = 1 + 20*m
-    let fit = (status.population[m].fit/worstFitness)*350 
+    let fit = (status.population[m].fit/worstFitness)*500 
     if(status.population[m].fit != bestFitness.value) 
       display.fillStyle = "#c1affa";
     else display.fillStyle = "#00FF00";
@@ -53,7 +53,7 @@ const currentGeneration = ref(0)
 const bestFitness = ref()
 const mutationRate = ref(0.5)
 const selectionSplit = ref(0.5)
-const commaJobs = ref()
+const commaJobs = ref("")
 const algoStep = ref()
 const isRunning = ref()
 
@@ -80,8 +80,13 @@ const setSelectionSplit = () => {
   GA.setSelectionSplit(selectionSplit.value)
 }
 const setJobsDuration = () => {
-  let jd = commaJobs.value.split(",")
-  GA.setJobDurations(jd)
+  let jdString = commaJobs.value
+  const checkValidity = /^[0-9]+(,[0-9]+)*$/g
+  if(jdString.match(checkValidity)){
+    let jd = jdString.split(",")
+    GA.setJobDurations(jd)
+  }
+  
 }
 
 
@@ -118,50 +123,97 @@ const reset = () => {
   GA.reset()
   
 }
-
+const durationValidity = computed(()=>{
+  console.log("asdasdsa")
+  const checkValidity = /^[0-9]+(,[0-9]+)*$/g
+  if(commaJobs.value.match(checkValidity))
+    return true
+  return false
+})
 const disablePopulation = computed(()=>{
   if(jobsNumber.value > 0 && machinesNumber.value > 0 && !isRunning.value)
     return false
   
   return true
 })
+
+const runDisabled = computed(()=>{
+  if(jobsNumber.value > 0 && 
+    machinesNumber.value > 0 && 
+    !isRunning.value &&
+    durationValidity.value && 
+    popSize.value > 0
+    )
+    return false
+  
+  return true
+})
+
+const stopDisabled = computed(()=>{
+  if(isRunning.value)
+    return false
+  
+  return true
+})
+
+const resetDisabled = computed(()=>{
+  if(isRunning.value)
+    return true
+  
+  return false
+})
+
+const isMemberSelected = (memberIndex)=>{
+  return memberIndex < popSize.value*selectionSplit.value
+}
+
 </script>
 
 <template>
   
   <div class="card">
-    <p class="panel-step-1">     
+    <div class="panel-step-1">     
       <div class="flex">
-        <div>
+        <div class="mr">
           <p class="label"> Jobs quantity:</p>
-          <input type="number" class ="input-ga" @input="setJobsNumber" v-model="jobsNumber" placeholder="job number" :disabled="isRunning">
+          <input type="number" class ="input-ga" @input="setJobsNumber" v-model="jobsNumber" placeholder="job number" :disabled="isRunning"  min="0" max="1000">
           <!-- <span>{{jobs}}</span> -->
         </div>
         <div v-if="jobsNumber > 0">
           <p class="label"> Comma separated jobs durations:</p>
-          <input type="text" style="width:400px" class ="input-ga" @input="setJobsDuration" v-model="commaJobs" placeholder="comma separated jobs duration" :disabled="isRunning">
+          <div>
+            <input type="text" style="width:400px" class ="input-ga"  :class="{ invalid: !durationValidity }" @input="setJobsDuration" v-model="commaJobs" placeholder="comma separated jobs duration" :disabled="isRunning">
+          </div>
         </div>
       </div> 
-      <p class="label"> Machines quantity:</p>
-      <input type="number" class ="input-ga" @input="setMachines" v-model="machinesNumber" placeholder="machines number">
-      
-      <p class="label"> Generations number:</p>
-      <input type="number" class ="input-ga" @input="setGenerations" v-model="generationNumber" placeholder="generation number" :disabled="isRunning">
-      
-      <p class="label"> Population size:</p>
-      <input type="number" class ="input-ga" @input="setPopulation" v-model="popSize" placeholder="population size" :disabled="disablePopulation">
-      
-      <p class="label"> Mutation rate: {{ mutationRate }}</p>
-      <input type="range" @input="setMutationRate" v-model="mutationRate"  min="0" max="1" step="0.01">
-      
-      <p class="label"> Selection split: {{ selectionSplit }}</p>
-      <input type="range" @input="setSelectionSplit" v-model="selectionSplit"  min="0" max="1" step="0.01">
-    </p>
+      <div class="flex justify">
+        <div>
+          <p class="label"> Machines quantity:</p>
+          <input type="number" class ="input-ga" @input="setMachines" v-model="machinesNumber" placeholder="machines number">
+        </div>
+        <div>
+          <p class="label"> Generations number:</p>
+          <input type="number" class ="input-ga" @input="setGenerations" v-model="generationNumber" placeholder="generation number" :disabled="isRunning">
+        </div>
+        <div>
+          <p class="label"> Population size:</p>
+          <input type="number" class ="input-ga" @input="setPopulation" v-model="popSize" placeholder="population size" :disabled="disablePopulation">
+        </div>
+        <div>
+          <p class="label"> Mutation rate: {{ mutationRate }}</p>
+          <input type="range" @input="setMutationRate" v-model="mutationRate"  min="0" max="1" step="0.01">
+        </div>
+        <div>
+          <p class="label selectedmember"> Selection split: {{ selectionSplit }}</p>
+          <input type="range" @input="setSelectionSplit" v-model="selectionSplit"  min="0" max="1" step="0.01">
+        </div>    
+      </div>
+    </div>
     <div class="flex justify">
       <div>
-        <button class="btn-cmnd" type="button" @click="run">run</button>
-        <button class="btn-cmnd" type="button" @click="stop">stop</button>
-        <button class="btn-cmnd" type="button" @click="reset">reset</button>
+        <button class="btn-cmnd" type="button" @click="run" :disabled="runDisabled" >run</button>
+        <button class="btn-cmnd" type="button" @click="stop" :disabled="stopDisabled">stop</button>
+        <button class="btn-cmnd" type="button" @click="reset" :disabled="resetDisabled">reset</button>
       </div>
       <div>
         <button class="btn-cmnd" type="button" @click="test">test problem</button>
@@ -176,11 +228,11 @@ const disablePopulation = computed(()=>{
     </div>
     <div class="displaypop">
       <div class="labels">
-        <p v-for="(item, index) in population">
+        <p v-for="(item, index) in population" :key="index"  :class="{selectedmember: isMemberSelected(index)}">
            {{ item.genes }}
         </p>  
       </div>
-      <canvas id="population-display" ref="canvas" width="350">
+      <canvas id="population-display" ref="canvas" width="500">
       </canvas>
       <div class="labels">
         <p v-for="(item, index) in population">
@@ -209,6 +261,7 @@ const disablePopulation = computed(()=>{
 }
 .displaypop{
   display: flex;
+  justify-content:center;
   margin-top: 10px;
   min-width: 600px;
 }
@@ -220,6 +273,8 @@ const disablePopulation = computed(()=>{
 }
 .panel-step-1{
   text-align: left;
+  margin: 15px 0px;
+  min-width: 1000px;
 }
 
 .input-ga{
@@ -241,10 +296,25 @@ const disablePopulation = computed(()=>{
   display: flex;
 }
 .details.flex span{
-  text-align: left;
+  text-align: center;
   flex-basis: 33%;
 }
 .justify{
   justify-content: space-between;
+}
+input.invalid{
+  border: solid 2px red;
+  color:red;
+}
+.selectedmember{
+  color: #f5a105;
+}
+
+.mr{
+  margin-right: 15px;
+}
+
+input{
+  border-radius: 5px;
 }
 </style>
